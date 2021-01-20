b60bba4502e3be4079c840543bb3a7509f632518
aguskoll <aguskoll@gmail.com>
2021-01-20 16:12:27 -0300

    circle around the marker code refactoring

--- app/src/main/java/com/rootstrap/android/ui/activity/main/targetpoint/MapFragment.kt
+++ app/src/main/java/com/rootstrap/android/ui/activity/main/targetpoint/MapFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,6 +12,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.rootstrap.android.R
@@ -42,34 +44,55 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
+    // todo remove example marker when we have the user location
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        addMarker()
+        addMarker(EXAMPLE_LAT, EXAMPLE_LONG)
     }
 
-    private fun addMarker() {
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions()
-            .position(sydney)
-            .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_map_location_marker))
+    private fun addMarker(latitude: Double, longitude: Double) {
+        val location = LatLng(latitude, longitude)
+        mMap.addMarker(
+            MarkerOptions()
+                .position(location)
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_map_location_marker))
         )
-        setMapCamera(sydney)
+        drawCircle(location)
+        setMapCamera(location)
+    }
+
+    private fun drawCircle(point: LatLng) {
+        val circleOptions = CircleOptions().apply {
+            center(point)
+            radius(CIRCLE_RADIUS)
+            strokeColor(ContextCompat.getColor(requireContext(), R.color.colorAccent))
+            fillColor(ContextCompat.getColor(requireContext(), R.color.whiteTransparent))
+            strokeWidth(CIRCLE_STROKE_WIDTH)
+        }
+
+        mMap.addCircle(circleOptions)
     }
 
     private fun setMapCamera(latLng: LatLng) {
-        val cameraPosition = CameraPosition.Builder()
-            .target(latLng)
-            .zoom(GOOGLE_MAPS_ZOOM)
-            .bearing(GOOGLE_MAPS_BEARING)
-            .tilt(GOOGLE_MAPS_TILT)
-            .build()
+        val cameraPosition = CameraPosition.Builder().apply {
+            target(latLng)
+            zoom(GOOGLE_MAPS_ZOOM)
+            bearing(GOOGLE_MAPS_BEARING)
+            tilt(GOOGLE_MAPS_TILT)
+        }.build()
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
     }
 
     companion object {
         const val GOOGLE_MAPS_ZOOM = 15f
         const val GOOGLE_MAPS_BEARING = 0f
-        const val GOOGLE_MAPS_TILT = 30f
+        const val GOOGLE_MAPS_TILT = 5f
+        const val CIRCLE_RADIUS = 90.0
+        const val CIRCLE_STROKE_WIDTH = 2f
+
+        // todo: remove this examples
+        const val EXAMPLE_LAT = -38.69
+        const val EXAMPLE_LONG = -62.25
 
         @JvmStatic
         fun newInstance() =
